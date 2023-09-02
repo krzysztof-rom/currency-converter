1575525ef4d42bd99b469507887cd842ce27e384
Krzysiek Romanowski <krzysztof_rom@o2.pl>
2023-09-02 22:01:14 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.47;
+let rateUSD = 4.12;
+let rateGBP = 5.2;
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
+
+  let result;
+
+  switch (currency) {
+    case "EUR":
+      result = amount / rateEUR;
+      break;
+
+    case "USD":
+      result = amount / rateUSD;
+      break;
+
+    case "GBP":
+      result = amount / rateGBP;
+      break;
+  }
+
+  resultElement.innerHTML = `${result.toFixed(2)}${currency}`;
+});
